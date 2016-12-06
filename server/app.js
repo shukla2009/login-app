704ed055b62f32113e163698c11f1647b7301afa
shuklara <rahul.shukla@synerzip.com>
2016-12-06 12:32:36 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,36 @@
+/**
+ * Main application file
+ */
+
+'use strict';
+import express from 'express';
+import mongoose from 'mongoose';
+mongoose.Promise = require('bluebird');
+import config from './config/environment';
+import http from 'http';
+
+// Connect to MongoDB
+mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+});
+
+
+// Setup server
+var app = express();
+var server = http.createServer(app);
+require('./express').default(app);
+require('./routes').default(app);
+
+// Start server
+function startServer() {
+  app.angularFullstack = server.listen(config.port, config.ip, function() {
+    console.log('Express server listening on %d, in %s mode', config.port);
+  });
+}
+
+setImmediate(startServer);
+
+// Expose app
+exports = module.exports = app;
---
+++ server/express.js
@@ -0,0 +1,42 @@
+/**
+ * Express configuration
+ */
+
+'use strict';
+
+import express from 'express';
+import morgan from 'morgan';
+import compression from 'compression';
+import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
+import cookieParser from 'cookie-parser';
+import config from './config/environment';
+import passport from 'passport';
+import session from 'express-session';
+import connectMongo from 'connect-mongo';
+import mongoose from 'mongoose';
+var MongoStore = connectMongo(session);
+
+export default function(app) {
+  app.use(morgan('dev'));
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.use(cookieParser());
+  app.use(passport.initialize());
+
+  // Persist sessions with MongoStore / sequelizeStore
+  // We need to enable sessions for passport-twitter because it's an
+  // oauth 1.0 strategy, and Lusca depends on sessions
+  app.use(session({
+    secret: config.secrets.session,
+    saveUninitialized: true,
+    resave: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      db: 'login'
+    })
+  }));
+
+}
